d9ce79de4187ea1b3f0f2a40593a69bc426eb9cd
Marc Parisi <phrocker@apache.org>
2020-02-16 19:49:04 -0500

    update combiner

--- README.md
+++ README.md
@@ -52,3 +52,7 @@ Note that the demo will not use provenance to track ingest. Instead, a flow will
 that receives data from your Jangowave app. Data will be ingested and queryable within a second. 
 
     setup.sh <accumulo instanceid> <zookeeperlist>
+
+It is important that the zookeeper list be an accessible IP. For example, if you are hosting on
+fluo-uno you may want to set your fluo uno hostname to 0.0.0.0 or an accessible adapter with an IP
+that will be accessible by the containers. 
--- jangowave/query/jsoncombiner.py
+++ jangowave/query/jsoncombiner.py
@@ -1,16 +1,26 @@
-from org.poma.accumulo import Key,KeyValue
+from pysharkbite import Key,KeyValue
 from org.apache.accumulo.core.data import Range,Value
 import json
 class PythonCombiner: 
 ## This example iterator assumes the CQ contains a field name
 ## and value pair separated by a null delimiter
+  def seek(self,iterator, range, families, inclusive):
+    iterator.seek(range,families,inclusive)
+    return self.onNext(iterator)
   def onNext(self,iterator):
     if (iterator.hasTop()):
         mapping = {}
         key = iterator.getTopKey()
+        prevkey = key
+        cfsplit = key.getColumnFamily().split("\x00")
+        mapping["shard"]=key.getRow()
+        if len(cfsplit) == 2:
+          mapping["datatype"]=cfsplit[0]
+          mapping["uid"]=cfsplit[1]
         cf = key.getColumnFamily()
         while (iterator.hasTop() and cf == key.getColumnFamily()):
             ## FN and FV in cq
+            prevkey = key
             split = key.getColumnQualifier().split('\x00');
             fieldvalue="EMPTY"
             if len(split) >= 1:
@@ -23,7 +33,8 @@ class PythonCombiner:
                 key = iterator.getTopKey()
         json_data = json.dumps(mapping,indent=4, sort_keys=True);
         value = Value(json_data);
-        kv = KeyValue(key,value)
+        skip_key = Key(prevkey.getRow(),prevkey.getColumnFamily(),prevkey.getColumnQualifier()+ "\uffff",prevkey.getColumnVisibility(),prevkey.getTimestamp())
+        kv = KeyValue(skip_key,value)
         return kv
     else: 
        return None
